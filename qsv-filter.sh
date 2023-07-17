1b6ea91e5b43c4a2fc18a61c19c72621f6d97cd3
Moss <mossman2142@gmail.com>
2023-07-17 21:40:37 +1200

    example to exclude internal transfers

--- qsv-filter.sh
+++ qsv-filter.sh
@@ -4,3 +4,8 @@
 qsv search --select 11 -i '.+' -o ${1}-transfers.csv ${1}
 # generate csv for external deposit and withdrawls
 qsv search --select 11 -i -v '.+' -o ${1}-externals.csv ${1}
+
+
+qsv search --select 3 -i '^TRANSFER FROM T M M CANTWELL.+' .\${1} -o .\${1}-exclude.csv
+
+qsv exclude 'Memo/Description' .\${1} 'Memo/Description' .\${1}-exclude.csv -o ${1}-without-transferfrom.csv
